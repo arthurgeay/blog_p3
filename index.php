badb2815cf927e05b3fcabbedb74f8d15e85c2dd
arthurgeay <arthurgeay.contact@gmail.com>
2017-06-04 19:00:25 +0200

    Isolation de l'affichage

--- index.php
+++ index.php
@@ -1,24 +1,7 @@
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <link href="blog.css" rel="stylesheet" />
-    <title>Blog - Home</title>
-</head>
-<body>
-    <header>
-        <h1>Blog</h1>
-    </header>
-    <?php
-    $bdd = new PDO('mysql:host=localhost;dbname=p3_blog;charset=utf8', 'root', 'root');
-    $articles = $bdd->query('select * from t_article order by art_id desc');
-    foreach ($articles as $article): ?>
-    <article>
-        <h2><?php echo $article['art_title'] ?></h2>
-        <p><?php echo $article['art_content'] ?></p>
-    </article>
-    <?php endforeach ?>
-    <footer class="footer">
-    </footer>
-</body>
-</html>
+<?php
+// Data access
+$bdd = new PDO('mysql:host=localhost;dbname=p3_blog;charset=utf8', 'root', 'root');
+$articles = $bdd->query('select * from t_article order by art_id desc');
+
+// Data display
+require 'view.php';
