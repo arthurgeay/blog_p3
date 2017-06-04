f8165fc124e4e750662c7f22071b10132e4efc2f
arthurgeay <arthurgeay.contact@gmail.com>
2017-06-04 19:20:24 +0200

    Ajout de la route (url racine) et modifs de l'index

--- web/index.php
+++ web/index.php
@@ -1,5 +1,12 @@
 <?php
 
-require 'model.php';
-$articles = getArticles();
-require 'view.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// enable the debug mode
+$app['debug'] = true;
+
+require __DIR__.'/../app/routes.php';
+
+$app->run();
