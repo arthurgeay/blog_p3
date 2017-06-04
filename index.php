5d2229814bd51e3ca850de8983cff3fb4fb4bd22
arthurgeay <arthurgeay.contact@gmail.com>
2017-06-04 18:55:15 +0200

    Affichage de la liste des articles

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <link href="blog.css" rel="stylesheet" />
+    <title>MicroCMS - Home</title>
+</head>
+<body>
+    <header>
+        <h1>MicroCMS</h1>
+    </header>
+    <?php
+    $bdd = new PDO('mysql:host=localhost;dbname=p3_blog;charset=utf8', 'root', 'root');
+    $articles = $bdd->query('select * from t_article order by art_id desc');
+    foreach ($articles as $article): ?>
+    <article>
+        <h2><?php echo $article['art_title'] ?></h2>
+        <p><?php echo $article['art_content'] ?></p>
+    </article>
+    <?php endforeach ?>
+    <footer class="footer">
+    </footer>
+</body>
+</html>
