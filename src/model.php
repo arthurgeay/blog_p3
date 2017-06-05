5665ac3647cc04f6eca3294fb657d184248758fd
arthurgeay <arthurgeay.contact@gmail.com>
2017-06-05 17:47:16 +0200

    Entité d'un commentaire

--- src/model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Return all articles
-function getArticles() {
-    $bdd = new PDO('mysql:host=localhost;dbname=p3_blog;charset=utf8', 'root', 'root');
-    $articles = $bdd->query('select * from t_article order by art_id desc');
-    return $articles;
-}
