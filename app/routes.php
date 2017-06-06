98f180e97f78bb92b0c99f35c443f1304d6bb611
arthurgeay <arthurgeay.contact@gmail.com>
2017-06-06 16:08:06 +0200

    Modifications architecture

--- app/routes.php
+++ app/routes.php
@@ -2,6 +2,11 @@
 
 // Home page
 $app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig');
+});
+
+// Blog page
+$app->get('/blog', function () use ($app) {
     $articles = $app['dao.article']->findAll();
-    return $app['twig']->render('index.html.twig', array('articles' => $articles));
+    return $app['twig']->render('blog.html.twig', array('articles' => $articles));
 });
