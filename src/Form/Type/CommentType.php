4495095ff09fea75d2e3faf240331c3fb1306591
arthurgeay <arthurgeay.contact@gmail.com>
2017-06-22 01:13:38 +0200

    Essai de fonctionnalité de réponse commentaire

--- src/Form/Type/CommentType.php
+++ src/Form/Type/CommentType.php
@@ -6,11 +6,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class CommentType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
+    	$builder->add('parent_id', HiddenType::class);
     	$builder->add('author', TextType::class);
         $builder->add('content', TextareaType::class);
     }
